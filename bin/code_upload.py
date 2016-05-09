47624719fd62d0817acee5d34cabede3bc70105b
xuning <xuning@10-10-56-134.(none)>
2016-05-09 17:37:56 +0800

    参数

---
+++ bin/code_upload.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+
+
+
+from optparse import OptionParser
+
+
+
+
+#定义参数格式
+
+usage ="usage: %prog [options] arg1 agr2"
+parser =OptionParser(usage=usage)
+parser.
+(options, args) = parser.parse_args()
+
