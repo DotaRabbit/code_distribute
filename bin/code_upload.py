019e6356925ee222f7114da93bd303a24401e377
xuning <xuning@10-10-56-134.(none)>
2016-05-10 18:03:34 +0800

    2016-5-10

--- bin/code_upload.py
+++ bin/code_upload.py
@@ -5,16 +5,38 @@
 
 
 
-
 from optparse import OptionParser
-
-
+from fabric.api import local,lcd,run,env,hosts,execute,roles,put
+import ConfigParser
+import logging
 
 
 #定义参数格式
 
-usage ="usage: %prog [options] arg1 agr2"
-parser =OptionParser(usage=usage)
-parser.
+usage = "usage: %prog [options] arg1 agr2"
+parser = OptionParser(usage=usage)
 (options, args) = parser.parse_args()
+
+
+
+
+
+#配置文件
+conf = ConfigParser.ConfigParser()
+conf.read('../conf/upload.conf')
+sections = conf.sections()
+print sections
+ip = conf.get('host1','ip')
+print ip
+
+
+#定义日志
+
+
+
+
+
+
+
+
 
