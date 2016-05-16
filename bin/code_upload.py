0b84b1ef8c7e4012adc925df7c07766b091ce53f
DotaRabbit <349470670@qq.com>
2016-05-16 19:53:48 +0800

    2016-5-16-v1.0

--- bin/code_upload.py
+++ bin/code_upload.py
@@ -9,6 +9,9 @@ from optparse import OptionParser
 from fabric.api import *
 import ConfigParser
 import logging
+from progressbar import *
+import os
+from multiprocessing import Process
 
 
 #定义参数格式
@@ -22,21 +25,26 @@ import logging
 
 
 
-#配置文件
-conf = ConfigParser.ConfigParser()
-conf.read('../conf/upload.conf')
-sections = conf.sections()
+#配置文件信息
+host_conf = ConfigParser.ConfigParser()
+host_conf.read('../conf/host.conf')
+host_sections = host_conf.sections()
+
+deploy_conf = ConfigParser.ConfigParser()
+deploy_conf.read('../conf/deploy.conf')
+deploy_sections = deploy_conf.sections()
+
 env.passwords = {}
 env.hosts = []
 env.roledefs = {}
 
 
-for host_info in sections:
-    host = conf.get(host_info,'host')
-    port = conf.get(host_info,'port')
-    user = conf.get(host_info,'user')
-    password = conf.get(host_info,'password')
-    role = conf.get(host_info,'role')
+for host_info in host_sections:
+    host = host_conf.get(host_info,'host')
+    port = host_conf.get(host_info,'port')
+    user = host_conf.get(host_info,'user')
+    password = host_conf.get(host_info,'password')
+    role = host_conf.get(host_info,'role')
 
     env.hosts.append('%s@%s:%s'%(user,host,port))
     env.passwords['%s@%s:%s'%(user,host,port)] = password
@@ -50,25 +58,46 @@ for host_info in sections:
 
 
 
-print env.roledefs
+
+
 
 #定义日志
 
 
 
 
+file_size = os.path.getsize('C:\Users\cnbjpuhui-384\Desktop\package\puhui-uc-server-restful.zip')
 
-
-
-
-a='webserver'
-@roles('%s' %a)
-def web():
-    run('ls -l')
+print type(file_size)
 
 
 
 
 
 
-execute(web)
+#进度条
+@roles('webserver')
+def upload():
+    widgets = [Bar('#'), ' ', 'Uploading:', Percentage(), ' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=file_size)
+    print file_size
+
+    pbar.start()
+    def put_process():
+        put('C:\Users\cnbjpuhui-384\Desktop\package\puhui-uc-server-restful.zip', '/home/xuning/a.zip')
+    p=Process(target = put_process)
+    p.start()
+    p.join()
+    size = 0
+
+
+    while size <= file_size:
+
+        time.sleep(1)
+        size = run("ls -l /home/xuning/a.zip|awk '{print $5}'")
+        print size
+        pbar.update(size)
+
+
+    pbar.finish()
+execute(upload)
